f59ce194fb9634032c8b023bdfb67e593a6dcca1
Olanrewaju Toyyib Olaitan <olarewajutoyyib@gmail.com>
2026-03-15 00:34:51 +0100

    Update and rename .env.ts to env.ts

---
+++ src/env.ts
@@ -0,0 +1,7 @@
+export const env = {
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "",
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || "",
+
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID || "",
+  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET || "",
+}
