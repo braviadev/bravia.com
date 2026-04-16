d8bcc1f38f1389b271773e6db1e7313747abfd42
Olanrewaju Toyyib <olarewajutoyyib@gmail.com>
2026-04-17 00:04:34 +0100

    Update progress-bar.tsx

--- src/components/blog/progress-bar.tsx
+++ src/components/blog/progress-bar.tsx
@@ -1,16 +1,58 @@
 'use client'
 
-import { motion, useScroll } from 'motion/react'
+import type { TOC } from '@/mdx-plugins'
 
-export function ProgressBar() {
-  const { scrollYProgress } = useScroll()
+import { AlignLeftIcon } from 'lucide-react'
+import { useTranslations } from 'next-intl'
+import { useState } from 'react'
+
+import { Button } from '@/components/ui/button'
+import { Link } from '@/components/ui/link'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { useRouter } from '@/i18n/routing'
+
+type MobileTableOfContentsProps = {
+  toc: TOC[]
+}
+
+export function MobileTableOfContents(props: MobileTableOfContentsProps) {
+  const { toc } = props
+  const [isOpen, setIsOpen] = useState(false)
+  const router = useRouter()
+  const t = useTranslations()
 
   return (
-    <motion.div
-      className='fixed inset-x-0 top-0 z-50 h-1 origin-left bg-foreground'
-      style={{
-        scaleX: scrollYProgress,
-      }}
-    />
+    <Popover open={isOpen} onOpenChange={setIsOpen}>
+      <PopoverTrigger
+        className='gap-2'
+        render={
+          <Button variant='secondary' className='fixed right-2 bottom-2 z-50 lg:hidden'>
+            <AlignLeftIcon data-icon='inline-start' /> {t('blog.on-this-page')}
+          </Button>
+        }
+      />
+      <PopoverContent align='end' side='top' className='px-0 py-2'>
+        {toc.map((item) => {
+          const { title, url, depth } = item
+
+          return (
+            <Link
+              key={url}
+              href={{ hash: url }}
+              className='block py-2 pr-2.5 text-sm/tight text-muted-foreground transition-colors hover:text-foreground'
+              style={{
+                paddingLeft: (depth - 1) * 16,
+              }}
+              onClick={() => {
+                router.push(`#${url}`)
+                setIsOpen(false)
+              }}
+            >
+              {title}
+            </Link>
+          )
+        })}
+      </PopoverContent>
+    </Popover>
   )
 }
