98188032c8f905c1477540dde9be5dfccc142a8d
Olanrewaju Toyyib <olarewajutoyyib@gmail.com>
2026-04-17 01:26:14 +0100

    Add files via upload

--- src/tests/unit/components/home/hero.test.tsx
+++ src/tests/unit/components/home/hero.test.tsx
@@ -1,8 +1,8 @@
 import { screen } from '@testing-library/react'
 import { describe, expect, test } from 'vitest'
 
-import Hero from '@/components/home/hero'
-import { MY_NAME } from '@/lib/constants'
+import { Hero } from '@/components/home/hero'
+import { MY_NAME } from '@/constants/site'
 import { render } from '@/utils/render'
 
 describe('<Hero />', () => {
